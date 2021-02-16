89ead76287333151b92fbf78a6cbd39f490e94ea
Nina D. K <antwnina.k@gmail.com>
2021-02-16 11:54:05 +0200

    Fix and modify carLoanCalculator.js

--- lesson2/carLoanCalculator.js
+++ lesson2/carLoanCalculator.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-statements */
 //given: loan amount, APR, loan duration
 //return the monthly payment
 
@@ -9,36 +10,55 @@
 
 const rlSync = require("readline-sync");
 
+let positiveInput = input => {
+  while (!Number(input) || (Number(input) <= 0)) {
+    console.log("You must provide a positive number.");
+    input = rlSync.question();
+  }
+  return input;
+};
+
+let invalidInput = input => {
+  while ((!Number(input) && Number(input) !== 0) || (Number(input) < 0)) {
+    console.log("Please enter a non-negative number.");
+    input = rlSync.question();
+  }
+  return input;
+};
+
 console.log(`Welcome to Car Loan Calculator!
 ${"---".padStart((31 / 2) + 1)}`);
 carLoanCalculator();
 
 // eslint-disable-next-line max-lines-per-function
 function carLoanCalculator() {
-  let values = {
-    totalLoan: parseFloat(rlSync.question("Enter the total amount of your loan: ")),
-    APR: parseFloat(rlSync.question("Enter your APR in percentage form (i.e. 10 for 10%): ")),
-    durationInYears: parseInt(rlSync.question("Enter your loan duration. Years: "), 10),
-    durationInMonths: parseInt(rlSync.question("Months: "), 10)
-  };
-
-  for (const key in values) {
-    while ((!values[key] && values[key] !== 0) || values[key] < 0) {
-      console.log(`Please enter a valid number for ${key}.`);
-      values[key] = parseFloat(rlSync.question());
-    }
-  }
+  let totalLoan = Number(rlSync.question("Enter the total amount of your loan: "));
+  totalLoan = Number(positiveInput(totalLoan));
+  console.log(totalLoan, typeof totalLoan);
+  let APR = Number(rlSync.question("Enter your APR in percentage form (i.e. 10 for 10%): "));
+  APR = Number(invalidInput(APR));
+  let durationInYears = Number(rlSync.question("Enter your loan duration. Years: "));
+  durationInYears = Number(invalidInput(durationInYears));
+  let durationInMonths = Number(rlSync.question("Months: "));
+  durationInMonths = Number((durationInYears === 0) ?
+    positiveInput(durationInMonths) :
+    invalidInput(durationInMonths));
 
   let monthlyCost;
-  let duration = (values.durationInYears * 12) + values.durationInMonths;
-  if (values.APR === 0) monthlyCost = (values.totalLoan / duration).toFixed(2);
+  let duration = (durationInYears * 12) + durationInMonths;
+  if (APR === 0) monthlyCost = totalLoan / duration;
   else {
-    let monthlyIntRate = (values.APR / 100) / 12;
+    let monthlyIntRate = (APR / 100) / 12;
     // eslint-disable-next-line max-len
-    monthlyCost = values.totalLoan * (monthlyIntRate / (1 - Math.pow((1 + monthlyIntRate), (-duration))));
+    monthlyCost = totalLoan * (monthlyIntRate / (1 - Math.pow((1 + monthlyIntRate), (-duration))));
   }
   console.log(`Your monthly payment is: $${monthlyCost.toFixed(2)}.`);
   const again = rlSync.question("Do you want to perform another calculation? (yes/no)\n").toLowerCase();
   // eslint-disable-next-line no-unused-expressions
-  (again[0] === "y") ? carLoanCalculator() : console.log("Goodbye!");
+  if (again[0] === "y") {
+    console.clear();
+    carLoanCalculator();
+  } else {
+    console.log("Goodbye!");
+  }
 }
